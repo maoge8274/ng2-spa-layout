b48ad60a39a4a8bb44d61c1e8eecfcdffca9a1ca
leohe <leohe@leohedeMac-mini.local>
2017-05-23 15:14:26 +0800

    build

---
+++ lib/col.component.d.ts
@@ -0,0 +1,7 @@
+import { OnInit } from '@angular/core';
+import { ColBody } from './col-body.directive';
+export declare class Col implements OnInit {
+    body: ColBody;
+    constructor();
+    ngOnInit(): void;
+}
